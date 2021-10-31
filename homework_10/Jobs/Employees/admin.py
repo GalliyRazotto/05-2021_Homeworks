73a508bed3b8a9248a70425b23abc30d6cf54216
Kirill <yeahkirill@gmail.com>
2021-10-31 22:46:31 +0300

    hw10 deleted

--- homework_10/Jobs/Employees/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from Employees.models import Person
-
-admin.site.register(Person)
--- homework_10/Jobs/myauth/forms.py
+++
@@ -1,1 +0,0 @@
-from django.contrib.auth.forms import UserCreationFormfrom myauth.models import MyUserclass MyUserCreationForm(UserCreationForm):    class Meta:        model = MyUser        fields = ('username', 'email', 'password1', 'password2')
